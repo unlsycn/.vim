c6d516e8a2adf3fac2a075d44db2fd9de8ff08be
unlsycn <unlsycn@unlsycn.com>
2024-01-18 16:48:05 +0800

    feat: update vscode api

--- lua/config.lua
+++ lua/config.lua
@@ -7,8 +7,8 @@ opt.guicursor = { "i:hor20", "n-v:block" }
 
 --- key bindings ---
 -- windows
-keymap.set({"n", "x"}, "<C-w>t", function() require("vscode-neovim").notify('workbench.action.terminal.focus') end)
-keymap.set({"n", "x"}, "<C-w>e", function() require("vscode-neovim").notify('workbench.action.focusSideBar') end)
-keymap.set({"n", "x"}, "<C-w>p", function() require("vscode-neovim").notify('workbench.action.focusPanel') end)
+keymap.set({"n", "x"}, "<C-w>t", function() require("vscode-neovim").call('workbench.action.terminal.focus') end)
+keymap.set({"n", "x"}, "<C-w>e", function() require("vscode-neovim").call('workbench.action.focusSideBar') end)
+keymap.set({"n", "x"}, "<C-w>p", function() require("vscode-neovim").call('workbench.action.focusPanel') end)
 --actions
-keymap.set({"n", "x"}, "gr", function() require("vscode-neovim").notify('editor.action.referenceSearch.trigger') end)
+keymap.set({"n", "x"}, "gr", function() require("vscode-neovim").call('editor.action.goToReferences') end)
