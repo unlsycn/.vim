6e94d85324363f2c461a2a6c7283908b9a72d557
unlsycn <unlsycn@unlsycn.com>
2025-10-09 16:41:07 +0800

    fix(yank): prioritize osc52 when yanking
    Signed-off-by: unlsycn <unlsycn@unlsycn.com>

--- common.lua
+++ common.lua
@@ -1,38 +1,39 @@
 local copy = "wl-copy"
 local paste = "wl-paste"
-if vim.fn.executable(copy) == 1 and vim.fn.executable(paste) == 1 then
-	vim.opt.clipboard = "unnamedplus"
-	vim.g.clipboard = {
-		name = clip,
-		copy = {
-			["+"] = copy,
-			["*"] = copy,
-		},
-		paste = {
-			["+"] = paste .. " -n",
-			["*"] = paste .. " -n",
-		},
-		cache_enabled = true,
-	}
+if os.getenv('SSH_TTY') ~= nil then
+  function my_paste(reg)
+    return function(lines)
+      local content = vim.fn.getreg('"')
+      return vim.split(content, '\n')
+    end
+  end
+
+  vim.opt.clipboard:append("unnamedplus")
+  vim.g.clipboard = {
+    name = 'OSC 52',
+    copy = {
+      ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+      ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+    },
+    paste = {
+      ["+"] = my_paste(),
+      ["*"] = my_paste(),
+    },
+  }
 else
-	if os.getenv("SSH_TTY") ~= nil then
-		function my_paste(reg)
-			return function(lines)
-				local content = vim.fn.getreg('"')
-				return vim.split(content, "\n")
-			end
-		end
-		vim.opt.clipboard:append("unnamedplus")
-		vim.g.clipboard = {
-			name = "OSC 52",
-			copy = {
-				["+"] = require("vim.ui.clipboard.osc52").copy("+"),
-				["*"] = require("vim.ui.clipboard.osc52").copy("*"),
-			},
-			paste = {
-				["+"] = my_paste(),
-				["*"] = my_paste(),
-			},
-		}
-	end
+  if vim.fn.executable(copy) == 1 and vim.fn.executable(paste) == 1 then
+    vim.opt.clipboard = "unnamedplus"
+    vim.g.clipboard = {
+      name = clip,
+      copy = {
+        ["+"] = copy,
+        ["*"] = copy,
+      },
+      paste = {
+        ["+"] = paste .. " -n",
+        ["*"] = paste .. " -n",
+      },
+      cache_enabled = true,
+    }
+  end
 end
